4c6a7314927c961c38c9e6d0a2b4d112cda6fdf4
daze_77 <daze77@gmail.com>
2021-10-27 10:52:54 -0400

    updated profile image code to ensure picture fit in full container at all times and added margints to page outside of header and footer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './App.css';
+import './pages/Pages.css'
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import AboutPage from './pages/About'
 import ContactPages from './pages/Contact'
@@ -10,17 +11,17 @@ import MyFunction from './components/portfolioCard/portfolioCardSingle';
 
 function App() {
   return (
-      <div>
-        <MyFunction/>
+      <div >
+        {/* <MyFunction/> */}
         <Router>
           <NavBar/>
-
+          <div className="container">
             <Route exact path="/" component={AboutPage}/> 
             <Route exact path="/reactportfolio" component={AboutPage}/>
             <Route exact path="/about" component={AboutPage}/>
             <Route exact path="/Portfolio" component={PortfolioPages}/> 
             <Route exact path="/Contact" component={ContactPages}/>  
-
+          </div>
           <Footer/>
         </Router>
 
--- src/components/aboutCard/card.js
+++ src/components/aboutCard/card.js
@@ -1,30 +1,30 @@
 import React from 'react' 
 import ProfilePic from '../../../src/assets/images/IMG_1070.png'
+import './card.css'
 
 
 function Card(){
     return(
-    <section class="container-fluid">     
-        <main class="row pb-5">
-            <div class="col-12 col-md-8 py-5">
-                <div class="card mb-3" >
-                    <div class="row g-0">
-                        <div class="col-md-4">
-                        <img class=" img-fluid pe-3" src={ProfilePic} alt="..."/>
-                        </div>
-                        <div class="col-md-8">
-                            <div class="card-body">
-                                <h1 class="card-title lh-lg">About Me</h1>
-                                <p class="card-text">Full Stack Web Developer with a background in Project Management and a passion for learning. Effective at combining creativity and problem solving to develop user-friendly applications.  Known among staff and collogues for strong leadership, attention to detail and a positive attitude no matter the complexity of the problem or task.</p>
+        <section class="container-fluid">     
+            <main class="row pb-5">
+                <div class="col-12 col-md-8 py-5">
+                    <div class="card mb-3" >
+                        <div class="row g-0">
+                            <div class="col-md-4">
+                                <img class="img-fluid profilePic" src={ProfilePic} alt="..."/>
+                            </div>
+                            <div class="col-md-8">
+                                <div class="card-body">
+                                    <h1 class="card-title lh-lg">About Me</h1>
+                                    <p class="card-text">Full Stack Web Developer with a background in Project Management and a passion for learning. Effective at combining creativity and problem solving to develop user-friendly applications.  Known among staff and collogues for strong leadership, attention to detail and a positive attitude no matter the complexity of the problem or task.</p>
+                                </div>
                             </div>
                         </div>
                     </div>
-                  </div>
-            </div>     
-        </main>            
-    </section>    )
-    
-
+                </div>     
+            </main>            
+        </section>    
+    )
 }
 
 
