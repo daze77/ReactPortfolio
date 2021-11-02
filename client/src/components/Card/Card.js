2905ccc42b61b7554d391f45a8ef801b7da78e7d
daze_77 <daze77@gmail.com>
2021-11-02 11:14:31 -0400

    added alt tag to link for accessiblity

--- client/src/components/Card/Card.js
+++ client/src/components/Card/Card.js
@@ -11,7 +11,7 @@ return(
                 <div className ="card-body">
                     <h5 className ="card-title">{props.title}</h5>
                     <p className ="card-text">{props.description}</p>
-                    <a href={props.link} target="_blank" className ="">click here</a>
+                    <a href={props.link} alt={props.link} target="_blank" className ="">click here</a>
                 </div>
             </div>
         </div>
