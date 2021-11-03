9261b239300d03fc3849dc805a56aecbe511be97
daze_77 <daze77@gmail.com>
2021-11-03 14:11:43 -0400

    added some formatting to the portfolio page:

--- client/src/components/Card/Card.js
+++ client/src/components/Card/Card.js
@@ -6,15 +6,16 @@ function card(props){
 return(
     <>
       <div className="col">
+        <a href={props.link} alt={props.link} target="_blank" rel="noreferrer" className ="">
             <div className={`card h-100 ${props.class ? props.class : ""}`} style={{width: "100%"}} key={props.id}>
-                <img src={props.image} className="card-img-top" alt={props.image}/>
-                <div className ="card-body">
-                    <h5 className ="card-title">{props.title}</h5>
-                    <p className ="card-text">{props.description}</p>
-                    <a href={props.link} alt={props.link} target="_blank" className ="">click here</a>
+                    <img src={props.image} className="card-img-top" alt={props.image}/>
+                    <div className ="card-body">
+                        <h5 className ="card-title">{props.title}</h5>
+                        <p className ="card-text">{props.description}</p>
+                    </div>
+                    <div className="overlay"> <p>Click To Visit Page</p></div>
                 </div>
-                <div className="card-img-overlay"></div>
-            </div>
+            </a>
         </div>
     </>
 )
