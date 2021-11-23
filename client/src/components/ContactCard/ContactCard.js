292988d966fb60c29bed346071236b1318ca36cd
daze_77 <daze77@gmail.com>
2021-11-22 19:44:55 -0500

    updated linkedin link

--- client/src/components/ContactCard/ContactCard.js
+++ client/src/components/ContactCard/ContactCard.js
@@ -19,7 +19,7 @@ function Contactcard(){
                             <h1 className="lh-lg header">Contact</h1>
                             <div id="social" className=" px-3 pt-3" >
                                 <a  id="github" href="https://github.com/daze77" target="_blank" rel="noreferrer" ><i className="fab fa-github fa-2x"></i></a>
-                                <a href="https://www.linkedin.com/in/bill-hronis-8b9357155" target="_blank" rel="noreferrer"><i className="ps-2 fab fa-linkedin-in fa-2x "></i></a>
+                                <a href="https://www.linkedin.com/in/billhronis/" target="_blank" rel="noreferrer"><i className="ps-2 fab fa-linkedin-in fa-2x "></i></a>
                             </div>
                         </div>
                         <div className="row row-cols-1 row-cols-md-2 align-items-center">
