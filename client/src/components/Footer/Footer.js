1f344218bf448de859c97290c8b929da1b60c4cd
daze_77 <daze77@gmail.com>
2022-07-21 10:53:25 -0400

    corrected a word and updated year to auto update annually

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -3,19 +3,23 @@ import './Footer.css'
 
 
 function Footer(){
+
+const currentDate = new Date()
+const currentYear = currentDate.getFullYear()
+
+
+
+
+
     return(
     <>
-
-    <footer className="footer navbar navbar-light bg-secondary">
-        <div className="d-block text-center container-fluid">
-            <span className="navbar-brand mb-0"><small>&#169;Copyright 2021</small></span>
-        </div>
-    </footer>
+        <footer className="footer navbar navbar-light bg-secondary">
+            <div className="d-block text-center container-fluid">
+                <span className="navbar-brand mb-0"><small>&#169;Copyright {currentYear} </small></span>
+            </div>
+        </footer>
     </>
-
     )
-    
-
 }
 
 
