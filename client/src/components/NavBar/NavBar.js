18236f417929b7eb30270c182459e62bb5788c01
daze_77 <daze77@gmail.com>
2022-09-13 17:26:52 -0400

    fixed nav bar so that the color is changed on the active menu item

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -25,13 +25,13 @@ function NavBar(){
             <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
                 <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
                     <li className="nav-item">
-                        <NavLink to="/contact" className="nav-link active" aria-current="page" >Contact</NavLink>
+                        <NavLink to="/contact" className="nav-link" aria-current="page" >Contact</NavLink>
                     </li>
                     <li className="nav-item">
-                        <NavLink to="/portfolio" className="nav-link active">Portfolio</NavLink>
+                        <NavLink to="/portfolio" className="nav-link">Portfolio</NavLink>
                     </li>
                     <li className="nav-item">
-                        <NavLink to="/about" className="nav-link active">About</NavLink>
+                        <NavLink to="/about" className="nav-link">About</NavLink>
                     </li>
                 </ul>
             </div>
