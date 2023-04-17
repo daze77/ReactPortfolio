492912b94e007a9c893d1845667eff5d9aef8f08
daze_77 <daze77@gmail.com>
2023-04-17 18:41:17 -0400

    yet a few more adjustments to router

--- app/router.js
+++ app/router.js
@@ -12,17 +12,18 @@ function router(app){
 
     //this code is for the qtrade app - redirect to pass a value back to qtrade app server via params
      app.get('/api/qtradeCode/qtrade', (req, res) => { 
-        const code = req.query.code
-        console.log('hello are you there', req.query.code)
+        const code = req.query
+        console.log('hello are you there', req.query)
         x = code
 
                  
         res.json({code: code})
+        console.log('thisisxcode',x.code)
 
     })
 
     app.get('/api/qtrade', (req, res) => {
-        console.log('we got code', x)
+        console.log('we got code', x.code)
         res.json(x)
     })
 
