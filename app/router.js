419d22bbfd4f3c6411922b34c6adac161d4242d7
daze_77 <daze77@gmail.com>
2023-04-17 15:42:43 -0400

    using params to grab url and passing via variable x to new api route.  Hope this works

--- app/router.js
+++ app/router.js
@@ -3,29 +3,28 @@ require( 'dotenv' ).config()
 
 function router(app){
 
-    
+    let x = 
+
     app.get('/api/portfolio', function (req, res)  {
         res.json({token: process.env.TOKEN})
    
+    })
+
+    //this code is for the qtrade app - redirect to pass a value back to qtrade app server via params
+     app.get('/api/qtrade/:qtrade', (req, res) => { 
+                 x = req.params
+
+        res.json({res: req.params})
 
     })
 
-    // app.get('/api/qtrade', (req, res) => { 
-    //     console.log(req)
-    //    let x = req.url.split('=')[1]
-       
-    //     res.json({results: x})
-   
-    //     })
+    // this will take the params pushed to x and pass to new api route for access
+    app.get('/api/qtrade',(req,res) => {
+            console.log('did this work', x)
+            res.json(x)
+        })
 
-            app.get('/api/qtrade/:qtrade', (req, res) => { 
-    console.log(req.params)
-    // res.json({results: req.url})
-    //     // let x = req.url.split('=')[1]
-    //     // res.json({results: x})
-    res.json({res: req.params})
 
-    })
 
 
 }
