6a95bb4c30e0266f73c0ff5f14754d9dbfedf266
daze_77 <daze77@gmail.com>
2023-04-17 15:17:41 -0400

    small change to redirect link

--- app/router.js
+++ app/router.js
@@ -10,11 +10,22 @@ function router(app){
 
     })
 
-    app.get('/api/qtrade/', (req, res) => { 
-       let x = req.url.split('=')[1]
-        res.json({results: x})
+    // app.get('/api/qtrade', (req, res) => { 
+    //     console.log(req)
+    //    let x = req.url.split('=')[1]
+       
+    //     res.json({results: x})
    
-        })
+    //     })
+
+            app.get('/api/qtrade/:qtrade', (req, res) => { 
+    console.log(req.params)
+    // res.json({results: req.url})
+    //     // let x = req.url.split('=')[1]
+    //     // res.json({results: x})
+    res.json({res: req.params})
+
+    })
 
 
 }
